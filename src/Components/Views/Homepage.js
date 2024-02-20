8221e33e674ec4b959eedc641a8707dc0f0b594a
wizrahull <purirahul31@gmail.com>
2024-02-20 18:08:56 +0530

    login

--- src/Components/Views/Homepage.js
+++ src/Components/Views/Homepage.js
@@ -5,10 +5,5 @@ import { useAuth } from "../../Auth/AuthModule";
 export default function Homepage() {
   const { authUser, setAuthUser, isLogged, setIslogged } = useAuth();
 
-  return (
-    <div>
-      Homepage
-      {JSON.stringify(authUser)}
-    </div>
-  );
+  return <div>Homepage</div>;
 }
